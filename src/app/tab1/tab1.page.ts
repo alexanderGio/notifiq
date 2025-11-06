9ec01e9281bb45ed6809b46999db6f62a559fc56
giovana <geoalexander2222@gmail.com>
2025-11-06 19:16:34 -0300

    v03

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonButton, IonButtons, IonDatetime, IonIcon} from '@ionic/angular/standalone';
+
+
+import { addIcons } from 'ionicons';
+import { heart, logoApple, settingsSharp, star, addCircleOutline, bookmark, checkbox } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton, IonButtons, IonDatetime,
+    IonIcon
+  ],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() {
+    addIcons({ heart, logoApple, settingsSharp, star, addCircleOutline, bookmark, checkbox});
+  }
+  
 }
